117835732e395e04f00706867305f22143ded347
hkreshock <hkreshock@gmail.com>
2022-09-05 18:57:00 -0700

    router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import Navigation from './components/navigation';
 import LandingPage from './components/landingPage';
 import Footer from './components/footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexFlow: 'column', height: '100vh' }}>
-      <div style={{ height: '10vh', background: '#121212' }}>
-        <Navigation />
-      </div>
-      <div style={{ flex: '1 1 auto', background: '#323232' }}>
-        <LandingPage />
-      </div>
-      <div style={{ height: '10vh', background: '#121212' }}>
-        <Footer />
-      </div>
+    <div className="App">
+      <Router>
+        <Switch>
+         <Route path='/SoupTroop' exact component={(
+            <div style={{ display: 'flex', flexFlow: 'column', height: '100vh' }}>
+              <div style={{ height: '10vh', background: '#121212' }}>
+                <Navigation />
+              </div>
+              <div style={{ flex: '1 1 auto', background: '#323232' }}>
+                <LandingPage />
+              </div>
+              <div style={{ height: '10vh', background: '#121212' }}>
+                <Footer />
+              </div>
+            </div>
+         )}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
